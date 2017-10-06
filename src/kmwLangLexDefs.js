90d5ab53043fe018259435ebc278f796c92277b3
jahwsuf <jahwsuf@gmail.com>
2017-10-06 21:06:16 +0700

    Implements tokenization for (most of) the Keyman keyboard language, along with a 'tokenization.html' page for testing the lexer rules.  Works on a real keyboard source file!

--- src/kmwLangLexDefs.js
+++ src/kmwLangLexDefs.js
@@ -4,41 +4,74 @@ keyman.language = typeof(keyman.language) == 'undefined' ? {} : keyman.language;
 keyman.language.initializeLexer = keyman.language["initializeLexer"] = function() {
     var lexer = new Lexer;
 
-    lexer.addRule(/0x[a-f\d]+/i, function (lexeme) {
-        return { 
-            type:"HEX", 
-            text:lexeme
-        };
-    });
+    var makeRule = function(regex, tokenType, text) {
+        if(typeof(text) != "undefined") {
+            lexer.addRule(regex, function (lexeme) {
+                return { 
+                    type:tokenType, 
+                    text:text
+                };
+            });
+        } else if(typeof(tokenType) != "undefined") {
+            lexer.addRule(regex, function (lexeme) {
+                return { 
+                    type:tokenType, 
+                    text:lexeme
+                };
+            });
+        } else {
+            lexer.addRule(regex, function (lexeme) {
+            }); 
+        }
+    }
 
-    lexer.addRule(/"[^"]*?"/i, function (lexeme) {
-        return {
-            type:"STRING",
-            text:lexeme
-        };
-    })
-
-    lexer.addRule(/'[^']*?'/i, function (lexeme) {
-        return {
-            type:"STRING",
-            text:lexeme
-        };
-    })
-
-    lexer.addRule(/[a-zA-Z_\d]+/i, function (lexeme) {
-        return {
-            type:"VAR",
-            text:lexeme
-        };
-    })
+    makeRule(/any/i, "any");
+    makeRule(/baselayout/i, "baselayout");
+    makeRule(/beep/i, "beep");
+    makeRule(/begin/i, "begin");
+    makeRule(/call/i, "call");    
+    makeRule(/context/i, "context");
+    makeRule(/deadkey/i, "deadkey");
+    makeRule(/dk/i, "deadkey", "deadkey");
+    makeRule(/group/i, "group");
+    makeRule(/if/i, "if");
+    makeRule(/index/i, "index");
+    makeRule(/layer/i, "layer");
+    makeRule(/match/i, "match");
+    makeRule(/nomatch/i, "nomatch");
+    makeRule(/notany/i, "notany");
+    makeRule(/nul/i, "nul");
+    makeRule(/outs/i, "outs");
+    makeRule(/platform/i, "platform");
+    makeRule(/reset/i, "reset");
+    makeRule(/return/i, "return");
+    makeRule(/save/i, "save");
+    makeRule(/set/i, "set");
+    makeRule(/store/i, "store");
+    makeRule(/use/i, "use");
+    makeRule(/using keys/i, "using keys");
     
-    lexer.addRule(/\s+/i, function (lexeme) {
-        // Absorb all whitespace.
-        // return {
-        //     type:"WHITESPACE",
-        //     text:lexeme
-        // };
-    })
+    makeRule(/\n/i, "\n");
+    makeRule(/\+/i, "+")
+    makeRule(/>/i, ">");
+    makeRule(/\(/i, "(");
+    makeRule(/\)/i, ")");
+    makeRule(/=/i, "=");
+    makeRule(/\[/i, "[");
+    makeRule(/\]/i, "]");
+    makeRule(/,/i, ",");
+
+    makeRule(/0?x[a-fA-F\d]+/i, "HEX");
+    makeRule(/U\+[a-fA-F\d]+/i, "UNICODE");
+    makeRule(/"[^"]*?"/i, "STRING");
+    makeRule(/'[^']*?'/i, "STRING");
+    makeRule(/\d+/, "INTEGER");
+    makeRule(/[^\S\n]+/i);
+    makeRule(/c.*?\n/i); // Comment.
+
+    makeRule(/[a-zA-Z_\d]*/i, "NAME");
+    makeRule(/&[a-zA-Z_][a-zA-Z_\d]*/i, "SYSTEM_STORE");
+    makeRule(/\$[a-zA-Z_][a-zA-Z_\d]*/i, "NAMED_CONST");
 
     return lexer;
 }
